f691f7f8be86ac68b9d0c6b69cf8ef8983d870fd
Ruben Cuello <kuelmusic@gmail.com>
2024-04-10 20:38:24 -0400

    08

--- problems/08-advanced-exponent.js
+++ problems/08-advanced-exponent.js
@@ -20,19 +20,7 @@ exponentiation in your solution.
 
 Examples:
 
-advancedExponent(2, 0); // 1
-advancedExponent(2, 1); // 2
-advancedExponent(2, 2); // 4
-advancedExponent(2, 3); // 8
-advancedExponent(2, 4); // 16
-advancedExponent(2, 5); // 32
-advancedExponent(2, 6); // 64
-advancedExponent(2, 7); // 128
-advancedExponent(2, 8); // 256
-advancedExponent(2, 9); // 512
-advancedExponent(2, 10); // 1024
-advancedExponent(2, 11); // 2048
-advancedExponent(2, 12); // 4096
+
 
 For each of the examples above, figure out how many times your code should
 be recursively calling `advancedExponent`. Find a way to visually see how many
@@ -41,10 +29,32 @@ times `advancedExponent` is being recursively called.
 
 
 function advancedExponent(b, n) {
-  // Your code here 
+  if(n === 0){
+    return 1
+  }else if(n === 1){
+    return b * n
+  }
+  if(n % 2 === 0){
+    return advancedExponent(b, n / 2) ** 2
+  }
+  else {
+    return b * (advancedExponent(b, (n - 1) / 2) ** 2)
+  }
 }
 
-
+advancedExponent(2, 0); // 1
+advancedExponent(2, 1); // 2
+console.log(advancedExponent(2, 2)); // 4
+// advancedExponent(2, 3); // 8
+console.log(advancedExponent(2, 4)); // 16
+console.log(advancedExponent(2, 5)); // 32
+// advancedExponent(2, 6); // 64
+// advancedExponent(2, 7); // 128
+// advancedExponent(2, 8); // 256
+// advancedExponent(2, 9); // 512
+// advancedExponent(2, 10); // 1024
+// advancedExponent(2, 11); // 2048
+// advancedExponent(2, 12); // 4096
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = advancedExponent;
